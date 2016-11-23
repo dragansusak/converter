7654740dfe32ccf3a67f2c468f9dd6a32d66b65e
Dragan Susak <dragan.susak@live.com>
2016-11-23 12:17:10 +0100

    db database as server start config

---
+++ converter-service/src/test/java/de/zooplus/converter/service/external/ExchangeRateServiceImplTest.java
@@ -0,0 +1,21 @@
+package de.zooplus.converter.service.external;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by Dragan Susak on 23-Nov-16.
+ */
+public class ExchangeRateServiceImplTest {
+
+    @Test
+    public void getExchangeRate() throws Exception {
+    }
+
+    @Test
+    public void getLatest() throws Exception {
+
+    }
+
+}
